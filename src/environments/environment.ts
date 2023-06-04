389052e2ad5dc101b2cc63111a7ee0b1e6901f31
Julia Georgiew <java2011@me.com>
2023-06-04 09:12:40 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'fbjgnotizapp',
+    appId: '1:41050262640:web:efcef3bf2019ea0d7b78b5',
+    storageBucket: 'fbjgnotizapp.appspot.com',
+    apiKey: 'AIzaSyAh-Tr_sNoV1e1y45KE8-yIF3-RuHNzg4k',
+    authDomain: 'fbjgnotizapp.firebaseapp.com',
+    messagingSenderId: '41050262640',
+  },
+};
